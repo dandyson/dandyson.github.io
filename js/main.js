c91ee46dedd311994f92e86e6b67bf630b0377a5
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-07-22 14:32:11 +0100

    scroll to top button added, cv changed

--- js/main.js
+++ js/main.js
@@ -84,3 +84,32 @@ portfolioBoxes.forEach(box => {
         text.classList.add('textHidden');
     });
 });
+
+// SCROLL TO TOP
+
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const scrollButton = document.querySelector('.scrollToTop');
+
+function scrollToTop() {
+    if (window.scrollY >= scrollButton.offsetTop) {
+        scrollButton.classList.add('scrollShow');
+    } else {
+        scrollButton.classList.remove('scrollShow');
+    }
+}
+
+window.addEventListener('scroll', debounce(scrollToTop));
